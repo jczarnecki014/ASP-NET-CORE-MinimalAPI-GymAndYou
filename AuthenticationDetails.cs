cb9c79de7eb298e4b201b3736602e1088f08505c
kubacz002 <106175428+kubacz002@users.noreply.github.com>
2023-03-16 10:03:39 +0100

    Add jwtToken-based authentication

---
+++ AuthenticationDetails.cs
@@ -0,0 +1,9 @@
+﻿namespace GymAndYou___MinimalAPI___Project
+{
+    public class AuthenticationDetails
+    {
+        public string JwtKey { get; set; }
+        public string JwtIssuer { get; set; }
+        public int JwtExpireDays { get; set; }
+    }
+}
--- Program.cs
+++ Program.cs
@@ -1,8 +1,14 @@
 using FluentValidation;
+using GymAndYou___MinimalAPI___Project;
 using GymAndYou___MinimalAPI___Project.Gyms;
 using GymAndYou___MinimalAPI___Project.Gyms.Validator;
+using Microsoft.IdentityModel.Tokens;
+using System.Text;
 
 var builder = WebApplication.CreateBuilder(args);
+
+var authentication = new AuthenticationDetails();
+builder.Configuration.GetSection("Authentication").Bind(authentication);
 
 // Add services to the container.
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
@@ -12,6 +18,28 @@ builder.Services.AddSwaggerGen();
 //Register services
 builder.Services.AddSingleton<GymService>();
 builder.Services.AddScoped<IValidator<Gym>,GymValidator>();
+builder.Services.AddSingleton(authentication);
+
+//Authentication
+
+    builder.Services.AddAuthentication(option => 
+    {
+        option.DefaultAuthenticateScheme = "Bearer";
+        option.DefaultScheme = "Bearer";
+        option.DefaultChallengeScheme = "Bearer";
+    }).AddJwtBearer(config => {
+        config.RequireHttpsMetadata = false;
+        config.SaveToken = true;
+        config.TokenValidationParameters = new TokenValidationParameters()
+        {
+            ValidIssuer = authentication.JwtIssuer,
+            ValidAudience = authentication.JwtIssuer,
+            IssuerSigningKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(authentication.JwtKey))
+        };
+    });
+//Authorization
+    builder.Services.AddAuthorization();
+    
 
 var app = builder.Build();
 
@@ -22,9 +50,14 @@ app.UseSwagger();
 app.UseSwaggerUI();
 }
 
+app.UseAuthentication();
+
 app.UseHttpsRedirection();
 
+app.UseAuthorization();
+
 app.RegisterEndpoints();
+app.RegisterAccountEndpoints();
 
 app.Run();
 
