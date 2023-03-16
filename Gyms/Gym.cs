0c254127bbc04fb1d9d305325be40546399144b5
kubacz002 <106175428+kubacz002@users.noreply.github.com>
2023-03-16 07:19:30 +0100

    Register Endpoints with openAPI documentation

---
+++ Gyms/Gym.cs
@@ -0,0 +1,9 @@
+﻿namespace GymAndYou___MinimalAPI___Project.Gyms;
+
+    public class Gym
+    {
+        public Guid Id { get; set; } = Guid.NewGuid();
+        public string Name { get; set; }
+        public bool isOpen { get;set; }
+    }
+
--- Program.cs
+++ Program.cs
@@ -1,9 +1,17 @@
+using FluentValidation;
+using GymAndYou___MinimalAPI___Project.Gyms;
+using GymAndYou___MinimalAPI___Project.Gyms.Validator;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+
+//Register services
+builder.Services.AddSingleton<GymService>();
+builder.Services.AddScoped<IValidator<Gym>,GymValidator>();
 
 var app = builder.Build();
 
@@ -16,29 +24,7 @@ app.UseSwaggerUI();
 
 app.UseHttpsRedirection();
 
-var summaries = new[]
-{
-    "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
-};
-
-app.MapGet("/weatherforecast", () =>
-{
-var forecast = Enumerable.Range(1, 5).Select(index =>
-    new WeatherForecast
-    (
-        DateOnly.FromDateTime(DateTime.Now.AddDays(index)),
-        Random.Shared.Next(-20, 55),
-        summaries[Random.Shared.Next(summaries.Length)]
-    ))
-    .ToArray();
-return forecast;
-})
-.WithName("GetWeatherForecast")
-.WithOpenApi();
+app.RegisterEndpoints();
 
 app.Run();
 
-internal record WeatherForecast(DateOnly Date, int TemperatureC, string? Summary)
-{
-    public int TemperatureF => 32 + (int)(TemperatureC / 0.5556);
-}
